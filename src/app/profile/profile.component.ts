d83baf70d5b2a45b9bb059770f84ef10d4cd8a0e
et-taousy <zouhirtaousy@gmail.com>
2018-05-02 10:27:27 +0200

    dive leaflet map

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -65,12 +65,12 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnDestroy() {
-    this.onDestroy$.next()
+    this.onDestroy$.next();
   }
 
   getPermit(id: number) {
     if (!this.user || this.user.id !== id /* && !this.user.isAdmin()) */) {
-      return null
+      return null;
     }
 
     const isFirefoxWithPdfJs = () => {
@@ -127,7 +127,7 @@ export class ProfileComponent implements OnInit {
       .take(1)
       .subscribe(data => {
         window.URL.revokeObjectURL(data.blobUrl)
-        this.saveBlob(this.permit.blob, this.permit.name)
+        this.saveBlob(this.permit.blob, this.permit.name);
       }
     )
   }
