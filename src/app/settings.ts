36fce3589733e3886c8c15c88fed81876a90d2aa
Vincent Bourgeos <vincent_bourgeois@natural-solutions.eu>
2018-05-22 17:20:58 +0200

    Update settings

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,7 +1,7 @@
 export var config = {
   //'serverURL' : 'http://149.202.44.29/capel',
-  'serverURL' : 'http://127.0.0.1:5000',
-  //'serverURL': 'https://capel-beta.herokuapp.com/',
+  //'serverURL' : 'http://127.0.0.1:5000',
+  'serverURL': 'https://capel-beta.herokuapp.com',
   'appName' : 'capel',
   'appVersion': '1.0'
 };
