6c60dafb4b3a8979ea522f00a983c5080ea1faae
et-taousy <zouhirtaousy@gmail.com>
2018-06-06 15:44:13 +0200

    some statistics

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,12 +10,17 @@ import {NgRedux} from '@angular-redux/store';
 export class ProfileComponent implements OnInit {
   user: any;
   dives: any[];
+  exploredSites: any = 0;
+  nbrDivesMonths: any = 0;
+  nbrHoursInWater: any = 0;
+  nbrDives: any = 0;
   constructor(private diveService: DiveService,
               private ngRedux: NgRedux<any>) {
 
     this.diveService.getDives().then(data => {
       this.dives = data;
-      console.log(data);
+      this.nbrDives = this.dives.length;
+      this.getNbrHoursInWaterAndNbrMonth();
     }, error => {
       console.log(error);
     });
@@ -26,6 +31,25 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+  getNbrHoursInWaterAndNbrMonth() {
+    let oldDive: any, startTime: any, endTime: any;
+    for ( const dive of this.dives) {
+      startTime = dive.times[0][0].split(':');
+      endTime = dive.times[0][1].split(':');
+      this.nbrHoursInWater += Math.abs(((( Number(endTime[0]) * 60 ) +  Number(endTime[1])) - (( Number(startTime[0]) * 60 ) + Number(startTime[1]))) / 60);
+      if ( ! oldDive ) {
+        this.nbrDivesMonths++;
+        oldDive = dive;
+      }else {
+        if ( new Date(dive.divingDate).getMonth() === new Date(oldDive.divingDate).getMonth() ) {
+          oldDive = dive;
+        }else {
+          this.nbrDivesMonths++;
+          oldDive = dive;
+        }
+      }
+    }
   }
 
 }
